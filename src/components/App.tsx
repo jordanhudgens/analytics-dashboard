1bd0dd55caf9e8799375da339a334a48c9c41f00
Jordan Hudgens <jordan@devcamp.com>
2021-02-08 11:43:29 -0700

    Built out the initial structure for the data call

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,30 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import Hello from "./Hello";
-import HelloWithHooks from "./HelloWithHooks";
+const API_TOKEN = "211e3ba5-273a-489c-a376-691c68db7527";
+const ENDPOINT = "https://devcamp.com/api/metrics/code_editor_grouped_by_day";
 
 const App = () => {
-  return (
-    <div>
-      <Hello compiler={"TS"} framework={"React"} />
-      <HelloWithHooks />
-    </div>
-  );
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios
+      .get(ENDPOINT, {
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${API_TOKEN}`,
+        },
+      })
+      .then((response: any) => {
+        console.log("DATA", response.data);
+      })
+      .catch((error) => {
+        console.log("ERROR GETTING DATA", error);
+      });
+  };
+  return <div>Coming soon...</div>;
 };
 
 export default App;
