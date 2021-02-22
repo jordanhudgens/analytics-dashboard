9dc245a43058b01dddd878701236b2ae8ed704fe
Jordan Hudgens <jordan@devcamp.com>
2021-02-22 11:34:54 -0700

    Created the initial activity chart component

---
+++ src/components/ActivityChart.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+
+export interface IChartSeriesItem {
+  name: string;
+  type: string;
+  data: number[];
+}
+
+interface IChartProps {
+  key?: string;
+  series: IChartSeriesItem[];
+  labels: string[];
+  name: string;
+}
+
+const LineChart = (props: IChartProps) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (props.series && props.labels) {
+      setData({
+        series: props.series,
+        options: {
+          chart: {
+            height: 420,
+            type: "line",
+            stacked: false,
+          },
+          stroke: {
+            width: [0, 2, 5],
+            curve: "smooth",
+          },
+          plotOptions: {
+            bar: {
+              columnWidth: "50%",
+            },
+          },
+
+          fill: {
+            opacity: [0.85, 0.25, 1],
+            gradient: {
+              inverseColors: false,
+              shade: "light",
+              type: "vertical",
+              opacityFrom: 0.85,
+              opacityTo: 0.55,
+              stops: [0, 2, 4, 6],
+            },
+          },
+          labels: props.labels,
+          markers: {
+            size: 0,
+          },
+          xaxis: {
+            type: "datetime",
+          },
+          yaxis: {
+            title: {
+              text: props.name,
+            },
+            min: 0,
+          },
+          tooltip: {
+            shared: true,
+            intersect: false,
+            y: {
+              formatter: function (y: any) {
+                if (typeof y !== "undefined") {
+                  return `${y.toFixed(0)} ${props.name}`;
+                }
+                return y;
+              },
+            },
+          },
+        },
+      });
+    }
+  }, [props.series, props.labels]);
+
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <div className="grid-1">
+      <Chart
+        options={data.options}
+        series={data.series}
+        type="line"
+        height={420}
+      />
+    </div>
+  );
+};
+
+export default LineChart;
